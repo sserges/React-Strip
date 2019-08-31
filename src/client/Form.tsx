cc9d472da605b1ac7e17085bebf0f7c5750edc74
sserges <agounkeserge@gmail.com>
2019-08-31 14:34:17 +0000

    eos account app created

--- src/client/Form.tsx
+++ src/client/Form.tsx
@@ -10,61 +10,114 @@ class Form extends React.Component<IFormProps, IFormState> {
     super(props);
     this.state = {
       name: "",
-      amount: ""
+      amount: "",
+      public_key: "",
+      account_name: "",
+      alert_message: ""
     };
   }
 
   handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      let { token } = await this.props.stripe.createToken({
-        name: this.state.name
-      });
-      let amount = this.state.amount;
-      await fetch("/api/donate", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify({ token, amount })
-      });
-      // redirect, clear inputs, thank alert
-      // console.log(token);
-    } catch (e) {
-      throw e;
+    if (
+      !this.state.name &&
+      !this.state.account_name &&
+      !this.state.public_key
+    ) {
+      this.setState({ alert_message: "All the fields are required" });
     }
+    // try {
+    //   let { token } = await this.props.stripe.createToken({
+    //     name: this.state.name
+    //   });
+    //   let amount = this.state.amount;
+    //   await fetch("/api/donate", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-type": "application/json"
+    //     },
+    //     body: JSON.stringify({ token, amount })
+    //   });
+    //   // redirect, clear inputs, thank alert
+    //   // console.log(token);
+    // } catch (e) {
+    //   throw e;
+    // }
   };
 
   render() {
     return (
       <main className="container">
+        <h1 style={{ textAlign: "center", fontFamily: "calibri light" }}>
+          <img src="" />
+          EOS Account Creation
+        </h1>
+        {this.state.alert_message ? (
+          <div className="alert alert-danger" role="alert">
+            {this.state.alert_message}
+          </div>
+        ) : null}
+
         <form
           className="form-group mt-3 border border-primary rounded shadow-lg p-3"
           onSubmit={this.handleSubmit}
         >
-          <label>Name</label>
-          <input
-            type="text"
-            className="input-group my-1 p-1 border border-dark"
-            value={this.state.name}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              this.setState({ name: e.target.value })
-            }
-          />
-          <label>Amount</label>
-          <input
-            type="text"
-            className="input-group my-1 p-1 border border-dark"
-            value={this.state.amount}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              this.setState({ amount: e.target.value })
-            }
-          />
-          <label>CC Number -- Exp. Date -- CVC</label>
-          <CardElement className="p-2 border border-dark" />
-          <button className="btn btn-primary border-dark  shadow mt-3">
-            Charge It!
-          </button>
+          <div className="form-group">
+            <label>Public Key</label>
+            <input
+              value={this.state.public_key}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                this.setState({ public_key: e.target.value })
+              }
+              className="form-control"
+              type="text"
+              placeholder="Enter your public key"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Account Name</label>
+            <input
+              value={this.state.account_name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                this.setState({ account_name: e.target.value })
+              }
+              className="form-control"
+              type="text"
+              placeholder="Enter your account name"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                this.setState({ name: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Amount</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.amount}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                this.setState({ amount: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <label>CC Number -- Exp. Date -- CVC</label>
+            <CardElement className="form-control" />
+          </div>
+
+          <button className="btn btn-primary">Charge It!</button>
         </form>
       </main>
     );
@@ -76,6 +129,9 @@ interface IFormProps extends ReactStripeElements.InjectedStripeProps {}
 interface IFormState {
   name: string;
   amount: string;
+  public_key: string;
+  account_name: string;
+  alert_message: string;
 }
 
 export default injectStripe(Form);
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as stripeLoader from "stripe";
+import axios from "axios";
 
 const router = express.Router();
 
@@ -21,11 +22,29 @@ const charge = (token: string, amt: number) => {
 router.post("/api/donate", async (req, res, next) => {
   try {
     let data = await charge(req.body.token.id, req.body.amount);
+    // console.log(typeof data);
     console.log(data);
     res.send("Charged!");
+    // const email = "agounkeserge@gmail.com";
+    // const password = "admin3004";
+    // const dataTosend = {
+    //   payment_details: data
+    // };
+    // axios
+    //   .post(`http://127.0.0.1:8000/dsv/payment/`, dataTosend, {
+    //     headers: {
+    //       authorization: "Token " + "b19e0a503528d1e8dfc3dc1384fd641b3a89ec16"
+    //     }
+    //   })
+    //   .then(response => {
+    //     console.log(response);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   } catch (e) {
     console.log(e);
-    res.status(500);
+    // res.status(500);
   }
 });
 
